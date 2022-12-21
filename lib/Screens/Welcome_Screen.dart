c61926acbfad9fe93066ac1e45ca19af797b16b2
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2022-12-21 19:17:16 +0200

    Mohamed Mostfa - adding picture and text to welcome screen

--- lib/Screens/Welcome_Screen.dart
+++ lib/Screens/Welcome_Screen.dart
@@ -14,6 +14,38 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/image1.jpeg",
+                height: 300,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Let's get started",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Never a better time than now to start",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black38,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
