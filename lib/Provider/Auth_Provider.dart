266043a4d0c7fdb0ef590235033a28aeeec01fdf
mohamed mostfa <mohamed.mostfa1970@gmail.com>
2023-01-09 15:43:20 +0200

    Mohamed Mostfa - provider creation

---
+++ lib/Provider/Auth_Provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthProvider extends ChangeNotifier {
+  bool _isSignedIn = false;
+  bool get isSignedIn => _isSignedIn;
+
+  AuthProvider() {
+    checkSignIn();
+  }
+
+  void checkSignIn() async {
+    final SharedPreferences s = await SharedPreferences.getInstance();
+    _isSignedIn = s.getBool("is_signin") ?? false;
+    notifyListeners();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:phone_auth_flutter_firebase/Screens/Welcome_Screen.dart';
+import 'package:phone_auth_flutter_firebase/provider/Auth_Provider.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,10 +16,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "FlutterPhoneAuth",
-      debugShowCheckedModeBanner: false,
-      home: WelcomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: const MaterialApp(
+        title: "FlutterPhoneAuth",
+        debugShowCheckedModeBanner: false,
+        home: WelcomeScreen(),
+      ),
     );
   }
 }
